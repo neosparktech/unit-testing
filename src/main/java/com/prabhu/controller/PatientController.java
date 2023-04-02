4a610b6898d6a5296306f3e55eecfb61003cfe2e
prabhushan <priyan.prabhu@gmail.com>
2023-04-01 22:35:28 -0400

    Changes

--- src/main/java/com/prabhu/controller/PatientController.java
+++ src/main/java/com/prabhu/controller/PatientController.java
@@ -1,14 +1,18 @@
 package com.prabhu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prabhu.patient.Patient;
 import com.prabhu.patient.PatientServices;
 
-@RestController("/patient")
+@RestController
+@RequestMapping("/patient")
 public class PatientController {
 	
 	@Autowired
@@ -19,6 +23,13 @@ public class PatientController {
 	public long addPatient(@RequestBody Patient patient) {
 		
 		return patientService.addPatient(patient);
+		
+	}
+	
+	@GetMapping("/get/{id}")
+	public Patient getPatient(@PathVariable("id") long id) {
+		
+		return patientService.getPatient(id);
 		
 	}
 	
--- src/test/java/com/prabhu/UnitTestingApplicationTests.java
+++ src/test/java/com/prabhu/UnitTestingApplicationTests.java
@@ -1,13 +1,31 @@
 package com.prabhu;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class UnitTestingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+//@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class UnitTestingApplicationTests {
+ 
+    @Autowired
+    private MockMvc mockMvc;
+ 
+    @Test
+    public void whenGetUsers_thenReturnJsonArray() throws Exception {
+        mockMvc.perform(get("/patient/get/3"))
+          .andExpect(status().isOk());
+        //  .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+         // .andExpect(jsonPath("$", hasSize(2)))
+//          .andExpect(jsonPath("$[0].username", is("john.doe")))
+//          .andExpect(jsonPath("$[1].username", is("jane.doe")));
+    }
 }
