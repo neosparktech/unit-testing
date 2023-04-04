968ab46b6992a94ace7cf061f0dad442d582cbf2
prabhushan <priyan.prabhu@gmail.com>
2023-04-03 21:20:25 -0400

    Changes

--- src/test/java/com/prabhu/AppointmentServiceTest.java
+++ src/test/java/com/prabhu/AppointmentServiceTest.java
@@ -1,5 +1,8 @@
 package com.prabhu;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -13,10 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.prabhu.patient.AppointmentServices;
 import com.prabhu.patient.Patient;
 
-import lombok.extern.slf4j.Slf4j;
-
 @ExtendWith(MockitoExtension.class)
-@Slf4j
 class AppointmentServiceTest {
 
 	@Spy
@@ -24,21 +24,29 @@ class AppointmentServiceTest {
 
 
 	@Test
-	void test_appointment_date_not_in_past()
-	{
-		Patient testPatient = Patient.builder().firstName("John").lastName("Doe").dateOfBirth(LocalDate.now()).build();
-		appointmentService.bookAppointment(testPatient, LocalDateTime.of(2023, 4, 2, 10, 00));
-	}
-
-	@Test
 	void test_appointment_date_in_past() {
 		Patient testPatient = Patient.builder().firstName("John").lastName("Doe").dateOfBirth(LocalDate.now()).build();
 
-		LocalDateTime fixedDateTime = LocalDateTime.of(2023, 4, 3, 10, 00);
-		try (MockedStatic<LocalDateTime> mocked = Mockito.mockStatic(LocalDateTime.class)) {
-			mocked.when(LocalDateTime::now).thenReturn(fixedDateTime);
+		LocalDateTime fixedDateTime = LocalDateTime.of(2023, 4, 2, 10, 00);
+		try (MockedStatic<LocalDateTime> mockedLocalDateTime = Mockito.mockStatic(LocalDateTime.class)) {
+			mockedLocalDateTime.when(LocalDateTime::now).thenReturn(fixedDateTime);
+
 			appointmentService.bookAppointment(testPatient, fixedDateTime);
 		}
+	}
+
+	@Test
+	void test_appointment_date_not_in_past()
+	{
+		Patient testPatient = Patient.builder().firstName("John").lastName("Doe").dateOfBirth(LocalDate.now()).build();
+
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			appointmentService.bookAppointment(testPatient, LocalDateTime.of(2023, 4, 2, 10, 00));
+
+		});
+
+		assertEquals("Appointment Datetime cannot be less than today date", exception.getMessage());
+
 	}
 
 }
