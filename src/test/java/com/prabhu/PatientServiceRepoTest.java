869501f1b9e629a65f1dc87a17a9e90e4f91e276
prabhushan <priyan.prabhu@gmail.com>
2023-11-18 22:37:31 -0500

    Changes

--- src/test/java/com/prabhu/PatientServiceRepoTest.java
+++ src/test/java/com/prabhu/PatientServiceRepoTest.java
@@ -1,16 +1,23 @@
 package com.prabhu;
 
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import com.prabhu.entities.Patient;
 import com.prabhu.repo.PatientRepo;
 
+import lombok.extern.slf4j.Slf4j;
+
 @DataJpaTest
+@Slf4j
 public class PatientServiceRepoTest {
 
 	@Autowired
@@ -21,7 +28,18 @@ public class PatientServiceRepoTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Patient patient = Patient.builder().name("prabhu").email("prabhu@test.com").build();
+		entityManager
+				.persist(patient);
+		Optional<Patient> optionalPatient = patientRepo.findById(patient.getId());
+		if (optionalPatient.isPresent()) {
+			assertTrue(optionalPatient.get().getName().equals("prabhu"));
+			log.info("Patient ID {} ", optionalPatient.get().getId());
+		}
+		else {
+			fail("No Patient found");
+		}
+		//fail("Not yet implemented");
 	}
 
 }
